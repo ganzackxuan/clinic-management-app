e489d32f6e4df86a432a5e77a7b5d4d5e0add587
ganzackxuan <zackxuangan0924@gmail.com>
2025-01-30 22:29:29 +0800

    Restructure

---
+++ app/pages/login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/globals.css";
+
+export default function Login() {
+  return (
+    <div className="loginContainer">
+      <text>Login</text>
+    </div>
+  );
+}
